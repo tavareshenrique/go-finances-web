b88dd75f68d74664fa6722368be86aacb499972b
Henrique Tavares <ihenrits@gmail.com>
2020-04-22 01:32:06 -0300

    Implementando formatValue

--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,4 +1,6 @@
 const formatValue = (value: number): string =>
-  Intl.NumberFormat().format(value); // TODO
+  Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' })
+    .format(value)
+    .replace(/^(\D+)/, '$1 ');
 
 export default formatValue;
