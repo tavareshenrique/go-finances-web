75e24770f69710cc22caa554b11f0b80708277c9
Henrique Tavares <ihenrits@gmail.com>
2020-04-22 01:32:53 -0300

    listando dados na tabela e nos cards

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,14 +1,19 @@
 import React, { useState, useEffect } from 'react';
 
-import income from '../../assets/income.svg';
-import outcome from '../../assets/outcome.svg';
-import total from '../../assets/total.svg';
+// eslint-disable-next-line import/no-duplicates
+import { format } from 'date-fns';
+// eslint-disable-next-line import/no-duplicates
+import pt from 'date-fns/locale/pt';
+
+import formatValue from '../../utils/formatValue';
 
 import api from '../../services/api';
 
 import Header from '../../components/Header';
 
-import formatValue from '../../utils/formatValue';
+import income from '../../assets/income.svg';
+import outcome from '../../assets/outcome.svg';
+import total from '../../assets/total.svg';
 
 import { Container, CardContainer, Card, TableContainer } from './styles';
 
@@ -30,15 +35,49 @@ interface Balance {
 }
 
 const Dashboard: React.FC = () => {
-  // const [transactions, setTransactions] = useState<Transaction[]>([]);
-  // const [balance, setBalance] = useState<Balance>({} as Balance);
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+  const [balance, setBalance] = useState<Balance>({} as Balance);
 
   useEffect(() => {
     async function loadTransactions(): Promise<void> {
-      // TODO
+      api.get('transactions').then(response => {
+        const transactionResponse = response.data.transactions.map(
+          (transaction: Transaction) => {
+            return {
+              ...transaction,
+              formattedValue:
+                transaction.type === 'outcome'
+                  ? `- ${formatValue(transaction.value)}`
+                  : formatValue(transaction.value),
+              formattedDate: format(
+                new Date(transaction.created_at),
+                'dd/MM/yyyy',
+                {
+                  locale: pt,
+                },
+              ),
+            };
+          },
+        );
+
+        setTransactions(transactionResponse);
+      });
+    }
+
+    async function loadBalance(): Promise<void> {
+      api.get('transactions').then(response => {
+        const balanceResponse = {
+          income: formatValue(response.data.balance.income),
+          outcome: formatValue(response.data.balance.outcome),
+          total: formatValue(response.data.balance.total),
+        };
+
+        setBalance(balanceResponse);
+      });
     }
 
     loadTransactions();
+    loadBalance();
   }, []);
 
   return (
@@ -51,51 +90,51 @@ const Dashboard: React.FC = () => {
               <p>Entradas</p>
               <img src={income} alt="Income" />
             </header>
-            <h1 data-testid="balance-income">R$ 5.000,00</h1>
+            <h1 data-testid="balance-income">{balance.income}</h1>
           </Card>
           <Card>
             <header>
               <p>Saídas</p>
               <img src={outcome} alt="Outcome" />
             </header>
-            <h1 data-testid="balance-outcome">R$ 1.000,00</h1>
+            <h1 data-testid="balance-outcome">{balance.outcome}</h1>
           </Card>
           <Card total>
             <header>
               <p>Total</p>
               <img src={total} alt="Total" />
             </header>
-            <h1 data-testid="balance-total">R$ 4000,00</h1>
+            <h1 data-testid="balance-total">{balance.total}</h1>
           </Card>
         </CardContainer>
 
-        <TableContainer>
-          <table>
-            <thead>
-              <tr>
-                <th>Título</th>
-                <th>Preço</th>
-                <th>Categoria</th>
-                <th>Data</th>
-              </tr>
-            </thead>
+        {transactions && (
+          <TableContainer>
+            <table>
+              <thead>
+                <tr>
+                  <th>Título</th>
+                  <th>Preço</th>
+                  <th>Categoria</th>
+                  <th>Data</th>
+                </tr>
+              </thead>
 
-            <tbody>
-              <tr>
-                <td className="title">Computer</td>
-                <td className="income">R$ 5.000,00</td>
-                <td>Sell</td>
-                <td>20/04/2020</td>
-              </tr>
-              <tr>
-                <td className="title">Website Hosting</td>
-                <td className="outcome">- R$ 1.000,00</td>
-                <td>Hosting</td>
-                <td>19/04/2020</td>
-              </tr>
-            </tbody>
-          </table>
-        </TableContainer>
+              {transactions.map(transaction => (
+                <tbody>
+                  <tr>
+                    <td className="title">{transaction.title}</td>
+                    <td className={transaction.type}>
+                      {transaction.formattedValue}
+                    </td>
+                    <td>{transaction.category.title}</td>
+                    <td>{transaction.formattedDate}</td>
+                  </tr>
+                </tbody>
+              ))}
+            </table>
+          </TableContainer>
+        )}
       </Container>
     </>
   );
